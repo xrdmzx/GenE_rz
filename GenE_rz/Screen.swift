b6d7150d2db8fd264c4d01cffd5d7ed88a9b5f2a
Ronald Z <xrdmzx@gmail.com>
2020-10-24 14:22:13 -0400

    scroll view added

--- GenE_rz/Screen.swift
+++ GenE_rz/Screen.swift
@@ -14,38 +14,44 @@ struct Screen: View {
     @Binding public var seqId: String
 
     var body: some View {
-        
-        VStack {
-            // this essentially replaces the former "Search" button (also plays sound)
-            Text("Results").onAppear{self.dataManager.downloadSeqData(seqId: self.seqId)}.onAppear(perform: playSound)
-            
-            Group {
-                Text(dataManager.dataSet?.INSDSeq.locus ?? "")
-                Text(dataManager.dataSet?.INSDSeq.organism ?? "")
-                Text(dataManager.dataSet?.INSDSeq.source ?? "")
-                Text(dataManager.dataSet?.INSDSeq.taxonomy ?? "")
-                Text("\(dataManager.dataSet?.INSDSeq.length ?? 0) ")
-                Text(dataManager.dataSet?.INSDSeq.moltype ?? "")
+        ScrollView {
+            ZStack {
+                // Background navy blue
+                Rectangle()
+                    .foregroundColor(Color(red: 10/255, green: 77/255, blue: 174/255)).edgesIgnoringSafeArea(.all)
+                VStack {
+                    //rz- this essentially replaces the former "Search" button (also plays sound) https://stackoverflow.com/questions/58883501/swiftui-calling-functions-from-other-class
+                    Text("Results").foregroundColor(Color.white).onAppear{self.dataManager.downloadSeqData(seqId: self.seqId)}.onAppear(perform: playSound).edgesIgnoringSafeArea(.all)
+                    
+                    Group {
+                        Text(dataManager.dataSet?.INSDSeq.locus ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.organism ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.source ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.taxonomy ?? "")
+                        Text("\(dataManager.dataSet?.INSDSeq.length ?? 0) ")
+                        Text(dataManager.dataSet?.INSDSeq.moltype ?? "")
+                    }.foregroundColor(Color.white)
+                    
+                    Group {
+                        Text(dataManager.dataSet?.INSDSeq.topology ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.strandedness ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.definition ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.featureTable.INSDFeature.map{ $0.INSDFeature_key}.joined(separator: ", \n") ?? "")
+                        Text(dataManager.dataSet?.INSDSeq.featureTable.INSDFeature.map{$0.INSDFeature_location}.joined(separator: ", \n") ?? "")
+                    }.foregroundColor(Color.white)
+                    
+                    Group {
+                        Text(dataManager.dataSet?.INSDSeq.sequence ?? "")
+                    }.foregroundColor(Color.white)
+                }
             }
-            
-            Group {
-                Text(dataManager.dataSet?.INSDSeq.topology ?? "")
-                Text(dataManager.dataSet?.INSDSeq.strandedness ?? "")
-                Text(dataManager.dataSet?.INSDSeq.definition ?? "")
-                Text(dataManager.dataSet?.INSDSeq.featureTable.INSDFeature.map{ $0.INSDFeature_key}.joined(separator: ", \n") ?? "")
-                Text(dataManager.dataSet?.INSDSeq.featureTable.INSDFeature.map{$0.INSDFeature_location}.joined(separator: ", \n") ?? "")
-            }
-            
-            Group {
-                Text(dataManager.dataSet?.INSDSeq.sequence ?? "")
-            }
-        }
+        }.frame(maxWidth: .infinity) //rz - allow for scrolling data by tapping anywhere on screen
     }
 }
 
 struct Screen_Previews: PreviewProvider {
     static var previews: some View {
-        // still not sure what the parameters in Screen() are for, but it allowed me to run my code without crashing... https://stackoverflow.com/questions/58701826/swiftui-how-to-instantiate-previewprovider-when-view-requires-binding-in-initia
+        //rz- still not sure what the parameters in Screen() are for, but it allowed me to run my code without crashing... https://stackoverflow.com/questions/58701826/swiftui-how-to-instantiate-previewprovider-when-view-requires-binding-in-initia
         Screen(seqId: .constant("text"))
     }
 }
