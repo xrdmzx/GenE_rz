fae509b7ab279bab09484ce872e6f9ae161a4fcc
Ronald Z <xrdmzx@gmail.com>
2020-10-27 22:43:35 -0400

    retrying comity after closing chris pull... added map view made some changes elsewhere to get it to work... still need to try and incorporate chris screen changes. -rz

---
+++ GenE_rz/ChrisUpdateScreen_gitPull.swift
@@ -0,0 +1,122 @@
+/*
+//
+//  ContentView.swift
+//  xml_parser
+//
+//  Created by Chris Richardson on 10/21/20.
+//
+
+import SwiftUI
+
+struct Screen: View {
+
+
+    @ObservedObject public var dataManager = DataManager.shared
+    @State private var seqId: String = ""
+
+    var body: some View {
+        VStack {
+           TextField("Enter Accession Number", text: $seqId)
+            .padding()
+
+        Button("Search") {
+            dataManager.downloadSeqData(seqId: seqId)
+               }
+
+            ScrollView {
+
+            VStack(alignment: .leading) {
+                VStack {
+
+                    Text(dataManager.dataSet?.INSDSeq.locus ?? "").padding()
+                    Text(dataManager.dataSet?.INSDSeq.organism ?? "").padding()
+                    Text(dataManager.dataSet?.INSDSeq.source ?? "").padding()
+                    Text(dataManager.dataSet?.INSDSeq.taxonomy ?? "").padding()
+                    Text("\(dataManager.dataSet?.INSDSeq.length ?? 0) ").padding()
+                    Text(dataManager.dataSet?.INSDSeq.strandedness ?? "").padding()
+                    Text(dataManager.dataSet?.INSDSeq.moltype ?? "").padding()
+
+                }
+
+                .cornerRadius(10)
+
+                VStack(spacing: 10) {
+                    ForEach(dataManager.featureList, id:\.self) { feature in
+
+                        VStack {
+                            VStack {
+                                Text(feature.INSDFeature_key).bold()
+                                Text(feature.INSDFeature_location)
+                            }.padding()
+
+                            IntervalSection(feature: feature)
+
+                            QualsSection(feature: feature)
+
+                        }
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .cornerRadius(10)
+                    }
+                }
+
+                .cornerRadius(20)
+            }
+            .padding(.horizontal)
+        }
+
+        }
+
+        }
+
+
+
+struct IntervalSection: View {
+    var feature: INSDFeature
+    var body: some View {
+        VStack {
+            ForEach(0..<feature.INSDFeature_intervals.count, id: \.self) { i in
+                ForEach(0..<feature.INSDFeature_intervals[i].INSDInterval.count, id: \.self) { j in
+                    if let from = feature.INSDFeature_intervals[i].INSDInterval[j].INSDInterval_from {
+                        VStack {
+                            Text("\(from)")
+                            Text("\(feature.INSDFeature_intervals[i].INSDInterval[j].INSDInterval_to ?? -1)")
+                        }
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width - 30)
+                        .background(Color.red.opacity(0.4))
+                        .cornerRadius(10)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct QualsSection: View {
+    var feature: INSDFeature
+    var body: some View {
+        VStack {
+            ForEach(0..<feature.INSDFeature_quals.count , id: \.self) { i in
+                ForEach(0..<feature.INSDFeature_quals[i].INSDQualifier.count , id: \.self) { j in
+                    VStack {
+                        Text(feature.INSDFeature_quals[i].INSDQualifier[j].INSDQualifier_name)
+                        Text(feature.INSDFeature_quals[i].INSDQualifier[j].INSDQualifier_value)
+                    }
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 30)
+                    .background(Color.blue.opacity(0.4))
+                    .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Screen()
+    }
+}
+}
+*/
